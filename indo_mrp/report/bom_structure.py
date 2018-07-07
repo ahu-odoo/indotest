bc11759b3c64c2aa5b7e293c7d712a774cdb0779
Antoine Huvelle <ahu@odoo.com>
2018-07-07 14:40:47 +0200

    [IMP] migrating cost structure report

--- indo_mrp/report/bom_structure.py
+++ indo_mrp/report/bom_structure.py
@@ -1,34 +1,11 @@
 ## -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
 
-from openerp.osv import osv
-from openerp.report import report_sxw
+from odoo import api, models
 
 
-class bom_structure(report_sxw.rml_parse):
-    def __init__(self, cr, uid, name, context):
-        super(bom_structure, self).__init__(cr, uid, name, context=context)
-        self.localcontext.update({
-            'get_children': self.get_children,
-        })
+class BomStructureReport(models.AbstractModel):
+    _name = 'report.mrp.report_mrpbomstructure'
+    _inherit = 'report.mrp.report_mrpbomstructure'
 
     def get_children(self, object, level=0):
         result = []
@@ -36,9 +13,17 @@ class bom_structure(report_sxw.rml_parse):
         def _get_rec(object, level):
             for l in object:
                 res = {}
-                res['pname'] = l.product_id.name
+                res['pname'] = l.product_id.name_get()[0][1]
                 res['pcode'] = l.product_id.default_code
-                res['pqty'] = l.product_qty
+                qty_per_bom = l.bom_id.product_qty
+                if uom:
+                    if uom != l.bom_id.product_uom_id:
+                        qty = uom._compute_quantity(qty, l.bom_id.product_uom_id)
+                    res['pqty'] = (l.product_qty *qty)/ qty_per_bom
+                else:
+                    #for the first case, the ponderation is right
+                    res['pqty'] = (l.product_qty *qty)
+                res['puom'] = l.product_uom_id
                 res['uname'] = l.product_uom.name
                 res['level'] = level
                 res['code'] = l.bom_id.code
@@ -49,22 +34,15 @@ class bom_structure(report_sxw.rml_parse):
                 res['code'] = l.bom_id.code
                 result.append(res)
                 if l.child_line_ids:
-                    if level<6:
+                    if level < 6:
                         level += 1
-                    _get_rec(l.child_line_ids,level)
-                    if level>0 and level<6:
+                    _get_rec(l.child_line_ids, level, qty=res['pqty'], uom=res['puom'])
+                    if level > 0 and level < 6:
                         level -= 1
             return result
 
         children = _get_rec(object,level)
 
         return children
-
-
-class report_mrpbomstructure(osv.AbstractModel):
-    _name = 'report.mrp.report_mrpbomstructure'
-    _inherit = 'report.abstract_report'
-    _template = 'mrp.report_mrpbomstructure'
-    _wrapped_report_class = bom_structure
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
